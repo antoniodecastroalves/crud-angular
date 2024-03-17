bfdcc332a1d1e7e0079ef8201a33ff123400632f
Antonio Alves <antoniodecastroalves@gmail.com>
2024-03-16 23:11:22 -0300

    :sparkles: feat: Creating Table of Materials to List Courses - 02

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
 
 @Component({
   selector: 'app-courses',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: any[] = [];
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end' }
+  ];
+  displayedColumns = ['name', 'catergory'];
 
   constructor() { }
 
